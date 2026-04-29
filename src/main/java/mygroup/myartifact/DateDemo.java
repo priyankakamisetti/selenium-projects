a2b77745496538194050541c26c1cbcbadf8d2f9
priyankakamisetti <priyankapriya523@gmail.com>
2026-04-29 21:45:54 +0530

    DateDemo

--- src/main/java/mygroup/myartifact/DateDemo.java
+++ src/main/java/mygroup/myartifact/DateDemo.java
@@ -4,11 +4,13 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DateDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
    WebDriver driver = new ChromeDriver();
 
@@ -23,28 +25,51 @@ public class DateDemo {
 	
 	String month=driver.findElement(By.className("ui-datepicker-month")).getText();
 	String year=driver.findElement(By.className("ui-datepicker-year")).getText();
-	if(month.equals("june") && year.equals("2026")) {
+	if(month.equals("June") && year.equals("2026")) {
+		driver.findElement(By.xpath("//a[text()='11']")).click();
 		break;
 	}
 	
 driver.findElement(By.xpath("//*[@title='Next']")).click();
 
-driver.findElement(By.xpath("//a[text()='11']")).click();
-}
-
-driver.findElement(By.id("datepicker2")).click();
-while(true) {
-	String month1=driver.findElement(By.className("ui-datepicker2-month")).getText();
-	String year1=driver.findElement(By.className("ui-datepicker2-year")).getText();
-	if(month1.equals("may")&& year1.equals("2026")) {
-		break;
-	}
-	driver.findElement(By.xpath("//*[@title='Next']")).click();
-	driver.findElement(By.xpath("//a[text()='5']")).click();
 
 }
+Thread.sleep(2000);
+driver.findElement(By.id("txtDate")).click();
+	Select monthDD =new Select(driver.findElement(By.className("ui-datepicker-month")));
+	monthDD.selectByVisibleText("Apr");
+	Select yearDD =new Select(driver.findElement(By.className("ui-datepicker-year")));
+	yearDD.selectByVisibleText("2025");
+	
+	if(driver.findElement(By.xpath("//a[@data-date="+15+"]")).isDisplayed()) {
+		driver.findElement(By.xpath("//a[@data-date="+15+"]")).click();
+		
+	}
+
+	
+	
+	Thread.sleep(2000);
+	driver.findElement(By.id("start-date")).sendKeys("20/9/2026");
+	driver.findElement(By.id("end-date")).sendKeys("26/9/2026");
+	Thread.sleep(1000);
+	driver.findElement(By.className("submit-btn")).click();
+	driver.close();
+	
+	driver.quit();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	}
+
 
 }
 
